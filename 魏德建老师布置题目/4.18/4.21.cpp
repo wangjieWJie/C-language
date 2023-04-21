b4e764a3cafc1d31d8bc07ae09ede9503be8115f
pamapama88 <3512232084@qq.com>
2023-04-21 23:02:25 +0800

    小问题

---
+++ 魏德建老师布置题目/4.18/4.21.cpp
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int a = 100;
+	int b = 0;
+	int c = 0;
+	int d = 0;
+	for (a = 100; a <= 999; a++)
+	{
+		b = a / 100;           /*百位数*/
+		c = a / 10 % 10;         /*十位数*/
+		d = a % 10;         /*个位数*/
+		if (a == b * b * b + c * c * c + d * d * d)
+		{
+			printf("我叫  %d\n", a);
+		}
+	}
+	return 0;
+
+}
