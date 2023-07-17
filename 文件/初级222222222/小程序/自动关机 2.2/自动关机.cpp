e3d394a80cc8ab33d5f98d677f6f5365192da7fe
wangjieWJie <3512232084@qq.com>
2023-07-17 19:57:50 +0800

    更新自动关机

--- 文件/初级222222222/小程序/自动关机 2.2/自动关机.cpp
+++ 文件/初级222222222/小程序/自动关机 2.2/自动关机.cpp
@@ -1,7 +1,9 @@
 /*
 日志
-1、在2.0版本中，只能输入整数分钟数和整数秒数，所以我想增加一个输入小数秒数来控制关机时间的功能，以便简化操作
-
+2.2 更新内容
+在2.1版本中，只能输入整数分钟数和整数秒数，所以我想增加一个输入小数秒数来控制关机时间的功能，以便简化操作
+由于命令里面只能包含整数，所以我只是修改了输入类型，让他支持小数了，但是只支持一位小数
+因为他是通过乘以六十来化整的，多余的小数会被自动类型转化给抹掉
 
 */
 
@@ -75,7 +77,7 @@ int main()
 	int www_error = 6;
 	while(www_error)
 	{
-		printf("请输入关机倒计时,单位：分钟 —> ");
+		printf("请输入关机倒计时,单位：分钟(最多小数点后面一位) —> ");
 		scanf("%f", &shut_time);
 
 		if (shut_time >= 0)
@@ -85,20 +87,25 @@ int main()
 		}
 	}
 
+	int int_time = shut_time;
+
 	system(char_num.c_str());
 
-again:
-	printf("i请注意你的电脑将在 %d 分钟后关机，如果输入我是猪则取消关机\n", shut_time);
-	getchar();                        //因为上面的scanf有残余的字符会导致下面input被输入字符，从而触发判断“不想活了”。所以要用一个这个来清除队列中残余的字符
-	scanf("%s", &input);
-	if (strcmp(input, "我是猪") == 0) //strcmp是用来专门比较两个字符串的，如果两个字符串相同，就输出0            在<sring.h>中
+	int again = 6;
+	while(again)
 	{
-		system("shutdown -a");
-	}
-	else
-	{
-		printf("不想活了？？\n");
-		goto again;
+		printf("i请注意你的电脑将在 %d 分钟 %.0f 秒后关机，如果输入我是猪则取消关机\n", int_time, (shut_time - int_time) * 60);
+		getchar();                        //因为上面的scanf有残余的字符会导致下面input被输入字符，从而触发判断“不想活了”。所以要用一个这个来清除队列中残余的字符
+		scanf("%s", &input);
+		if (strcmp(input, "我是猪") == 0) //strcmp是用来专门比较两个字符串的，如果两个字符串相同，就输出0            在<sring.h>中
+		{
+			system("shutdown -a");
+			again = 0;
+		}
+		else
+		{
+			printf("不想活了？？\n");
+		}
 	}
 	return 0;
 }
